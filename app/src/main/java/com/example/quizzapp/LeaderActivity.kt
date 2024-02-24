e35edc1a630d132dd6a0764e41fed8b970f0c32f
Mạnh <100762257+Qmanh1804@users.noreply.github.com>
2024-02-24 12:04:32 +0700

    create leaderActivity

---
+++ app/src/main/java/com/example/quizzapp/LeaderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quizzapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leader)
+    }
+}
--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.quizzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+import com.example.quizzapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val window:Window = this@MainActivity.window
+        window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.grey)
     }
 }
